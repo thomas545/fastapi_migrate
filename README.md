0b6d857db31fd76e035c01ecc2232301c0a6710d
Thomas Saied <tomas.temo77@gmail.com>
2022-06-18 14:47:02 +0200

    create first version of package

---
+++ README.md
@@ -0,0 +1,49 @@
+FastAPI Migrations
+=============
+
+FastAPI Migrations is an extension that handles SQLAlchemy database migrations for FastAPI applications using Alembic. The database operations are provided as command-line arguments under the `fastmigrate` command.
+
+Installation
+------------
+
+Install FastAPI Migrations with `pip`:
+
+    pip install fastapi_migrations
+
+Example
+-------
+
+Steps to handles database migrations through FastAPI Migrations:
+- Use `fastmigrate init` to generate the template and inialize the alembic template
+- don't forget to import meta data to `env.py` file
+- follow the FastAPI instructions to set the database configurations [reference](https://fastapi.tiangolo.com/advanced/sql-databases-peewee/)
+
+
+
+Create the database or enable migrations if the database already exists with the following command:
+
+    $ fastmigrate init
+
+You can then generate an initial migration:
+
+    $ fastmigrate migrate
+    
+The migration script needs to be reviewed and edited, as Alembic currently does not detect every change you make to your models. In particular, Alembic is currently unable to detect indexes. Once finalized, the migration script also needs to be added to version control.
+
+Then you can apply the migration to the database:
+
+    $ fastmigrate upgrade
+    
+Then each time the database models change repeat the `migrate` and `upgrade` commands.
+
+To sync the database in another system just refresh the `migrations` folder from source control and run the `upgrade` command.
+
+To see all the commands that are available run this command:
+
+    $ fastmigrate --help
+
+Resources
+---------
+
+- [Documentation]()
+- [pypi]() 
---
+++ migrations/main.py
@@ -0,0 +1,244 @@
+from functools import wraps
+import os
+import typer
+import logging
+import sys
+from alembic import __version__ as __alembic_version__
+from alembic.config import Config as AlembicConfig
+from alembic import command
+from alembic.util import CommandError
+
+app = typer.Typer(
+    add_completion=False,
+    help="FastAPI migrations made easy!",
+    name="FastAPI Migrations",
+)
+
+
+alembic_version = tuple([int(v) for v in __alembic_version__.split(".")[0:3]])
+log = logging.getLogger(__name__)
+
+
+class Config(AlembicConfig):
+    def __init__(self, *args, **kwargs):
+        self.template_directory = kwargs.pop("template_directory", None)
+        super().__init__(*args, **kwargs)
+
+    def get_template_directory(self):
+        if self.template_directory:
+            return self.template_directory
+        package_dir = os.path.abspath(os.path.dirname(__file__))
+        return os.path.join(package_dir, "templates")
+
+
+def catch_errors(f):
+    @wraps(f)
+    def wrapped(*args, **kwargs):
+        try:
+            f(*args, **kwargs)
+        except (CommandError, RuntimeError) as exc:
+            log.error("Error: " + str(exc))
+            sys.exit(1)
+
+    return wrapped
+
+
+@app.command()
+@catch_errors
+def list_templates():
+    """List available templates."""
+    config = Config()
+    config.print_stdout("Available templates:\n")
+    for tempname in sorted(os.listdir(config.get_template_directory())):
+        with open(
+            os.path.join(config.get_template_directory(), tempname, "README")
+        ) as readme:
+            synopsis = next(readme).strip()
+        config.print_stdout("%s - %s", tempname, synopsis)
+
+
+@app.command()
+@catch_errors
+def init(directory=None, multidb=False, template=None, package=False):
+    """Creates a new migration repository"""
+    if directory is None:
+        directory = "migrations"
+    template_directory = None
+    if template is not None and ("/" in template or "\\" in template):
+        template_directory, template = os.path.split(template)
+    config = Config(template_directory=template_directory)
+    config.set_main_option("script_location", directory)
+    config.config_file_name = os.path.join(directory, "alembic.ini")
+    if template is None:
+        template = "fastapi"
+    command.init(config, directory, template=template, package=package)
+
+
+@app.command()
+@catch_errors
+def revision(
+    directory=None,
+    message=None,
+    autogenerate=False,
+    sql=False,
+    head="head",
+    splice=False,
+    branch_label=None,
+    version_path=None,
+    rev_id=None,
+):
+    """Create a new revision file."""
+    opts = ["autogenerate"] if autogenerate else None
+    config = Config(directory, opts=opts)
+    command.revision(
+        config,
+        message,
+        autogenerate=autogenerate,
+        sql=sql,
+        head=head,
+        splice=splice,
+        branch_label=branch_label,
+        version_path=version_path,
+        rev_id=rev_id,
+    )
+
+
+@app.command()
+@catch_errors
+def migrate(
+    directory=None,
+    message=None,
+    sql=False,
+    head="head",
+    splice=False,
+    branch_label=None,
+    version_path=None,
+    rev_id=None,
+    x_arg=None,
+):
+    """Alias for 'revision --autogenerate'"""
+    config = Config(directory, opts=["autogenerate"], x_arg=x_arg)
+    command.revision(
+        config,
+        message,
+        autogenerate=True,
+        sql=sql,
+        head=head,
+        splice=splice,
+        branch_label=branch_label,
+        version_path=version_path,
+        rev_id=rev_id,
+    )
+
+
+@app.command()
+@catch_errors
+def edit(directory=None, revision="current"):
+    """Edit current revision."""
+    if alembic_version >= (0, 8, 0):
+        config = Config(directory)
+        command.edit(config, revision)
+    else:
+        raise RuntimeError("Alembic 0.8.0 or greater is required")
+
+
+@app.command()
+@catch_errors
+def merge(directory=None, revisions="", message=None, branch_label=None, rev_id=None):
+    """Merge two revisions together.  Creates a new migration file"""
+    config = Config(directory)
+    command.merge(
+        config, revisions, message=message, branch_label=branch_label, rev_id=rev_id
+    )
+
+
+@app.command()
+@catch_errors
+def upgrade(directory=None, revision="head", sql=False, tag=None, x_arg=None):
+    """Upgrade to a later version"""
+    config = Config(directory, x_arg=x_arg)
+    command.upgrade(config, revision, sql=sql, tag=tag)
+
+
+@app.command()
+@catch_errors
+def downgrade(directory=None, revision="-1", sql=False, tag=None, x_arg=None):
+    """Revert to a previous version"""
+    config = Config(directory, x_arg=x_arg)
+    if sql and revision == "-1":
+        revision = "head:-1"
+    command.downgrade(config, revision, sql=sql, tag=tag)
+
+
+@app.command()
+@catch_errors
+def show(directory=None, revision="head"):
+    """Show the revision denoted by the given symbol."""
+    config = Config(directory)
+    command.show(config, revision)
+
+
+@app.command()
+@catch_errors
+def history(directory=None, rev_range=None, verbose=False, indicate_current=False):
+    """List changeset scripts in chronological order."""
+    config = Config(directory)
+    if alembic_version >= (0, 9, 9):
+        command.history(
+            config, rev_range, verbose=verbose, indicate_current=indicate_current
+        )
+    else:
+        command.history(config, rev_range, verbose=verbose)
+
+
+@app.command()
+@catch_errors
+def heads(directory=None, verbose=False, resolve_dependencies=False):
+    """Show current available heads in the script directory"""
+    config = Config(directory)
+    command.heads(config, verbose=verbose, resolve_dependencies=resolve_dependencies)
+
+
+@app.command()
+@catch_errors
+def branches(directory=None, verbose=False):
+    """Show current branch points"""
+    config = Config(directory)
+    command.branches(config, verbose=verbose)
+
+
+@app.command()
+@catch_errors
+def current(directory=None, verbose=False):
+    """Display the current revision for each database."""
+    config = Config(directory)
+    command.current(config, verbose=verbose)
+
+
+@app.command()
+@catch_errors
+def stamp(directory=None, revision="head", sql=False, tag=None):
+    """'stamp' the revision table with the given revision; don't run any
+    migrations"""
+    config = Config(directory)
+    command.stamp(config, revision, sql=sql, tag=tag)
+
+
+def version_callback(value: bool):
+    if value:
+        version = "1.0.0"
+        typer.echo(f"version {version}")
+        raise typer.Exit()
+
+
+@app.callback()
+def main(
+    version: bool = typer.Option(
+        None,
+        "--version",
+        callback=version_callback,
+        is_eager=True,
+        help="Show the Manage FastAPI version information.",
+    )
+):
+    ...
